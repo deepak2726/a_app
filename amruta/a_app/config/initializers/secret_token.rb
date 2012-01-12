65e74d2253ab50d1011bb8a0d8f378f07aa991fa
deeapk2726 <deepak2726@gmail.com>
2012-01-12 17:20:26 +0530

    Initial commit

---
+++ amruta/a_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AApp::Application.config.secret_token = '90a28e7d19482b1deb0afe19f966e9079962bf226707bce2b2374dc0d13e343680193b53371f0936fea293da7a9fee17b49a381331c3a28f8d05a285626aa481'
